70b1600d865110baccdf81fa544e6a850da57f54
tca19 <30314448+tca19@users.noreply.github.com>
2018-03-14 11:27:13 +0100

    solve exercise 1.01

---
+++ exercise1.01.c
@@ -0,0 +1,8 @@
+/* tells compiler to include information about the standard input/output
+ * library */
+#include <stdio.h>
+
+main() /* define a function, no arguments received */
+{
+	printf("hello, world\n"); /* calls printf() function */
+}
