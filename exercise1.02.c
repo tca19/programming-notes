f2380bf4030330b99dab0501c3bc99e86d2c6fd9
tca19 <30314448+tca19@users.noreply.github.com>
2018-03-14 11:27:28 +0100

    solve exercise 1.02

---
+++ exercise1.02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+	printf("\t (horizontal tab)\n");
+	printf("\n (newline)\n");
+	printf("\a (\\a)\n");
+	printf("\b (backspace\b\b)\n");
+	printf("\\ (backslash)\n");
+	printf("\v (vertical tab)\n");
+	printf("\r (car\rriage return)\n");
+}
