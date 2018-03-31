798078be25fc4c9294b05d28aacaf5e61115b2dc
tca19 <30314448+tca19@users.noreply.github.com>
2018-03-31 22:02:01 +0200

    add explanations for exercise 3.04

--- ch03-control/exercises/exercise.3.04.c
+++ ch03-control/exercises/exercise.3.04.c
@@ -3,7 +3,6 @@
 #include <string.h>     /* strlen() */
 #include <stdlib.h>     /* abs() */
 
-
 void reverse(char s[])
 {
 	int c, i, j;
@@ -11,6 +10,20 @@ void reverse(char s[])
 	for (i = 0, j = strlen(s)-1; i < j; ++i, --j)
 		c = s[i], s[i] = s[j], s[j] = c;
 }
+
+/* In a two's complement number representation, numbers go from -2^(wordsize-1)
+ * to 2^(wordsize-1)-1. For wordize = 32, we hence have -(-2^31) = -2^31 (the
+ * same number). In that case, the variable n would still be negative after n =
+ * -n. So the do...while loop would stop after 1 iteration because n / 10 would
+ *  be negative (not correct because obviously we need more that 1 character to
+ *  represent -2^31). We can replace the condition by : "do ... while n is not
+ *  0".
+ *
+ * Moreover, for -2^31, the value (n%10) would also be negative and (n%10) + '0'
+ * might not be a valid digit. So we need to take the absolute value of (n%10)
+ * to handle that special case. This won't have any influence for all the other
+ * number (in their case, n is positive).
+ */
 
 void itoa(int n, char s[])
 {
