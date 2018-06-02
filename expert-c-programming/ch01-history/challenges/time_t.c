299e06279d4b9f32ef3e22cebb179cb1a3437ae7
tca19 <30314448+tca19@users.noreply.github.com>
2018-06-02 19:47:57 +0200

    Convert difftime result in year/month/day/hour...

--- expert-c-programming/ch01-history/challenges/time_t.c
+++ expert-c-programming/ch01-history/challenges/time_t.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <time.h>
 
+#define MAXLEN 80
+
 int main(void)
 {
-	time_t biggest, current;
-	double duration;
-	char s[100];
+	time_t biggest, now;
+	long duration;
+	char s[MAXLEN];
 
 	biggest = 0x7FFFFFFF;
-	current = time(NULL);
+	time(&now);
 
 	printf("Max time_t (local): %s", ctime(&biggest));
-	printf("Max time_t (UTC):   %s", asctime(gmtime(&biggest)));
+	printf("Max time_t (UTC):   %s\n", asctime(gmtime(&biggest)));
 
-	duration = difftime(biggest, current);
-	printf("Has to be cursed for %f seconds (~%f days)\n",
-	       duration, duration/86400);
+	duration = (long) difftime(biggest, now);
+	printf("The person who removed the comment will still be cursed for\n");
+
+	printf("  - %2ld years\n", duration/(3600*24*365));
+	duration %= (3600*24*365);
+	printf("  - %2ld months\n", duration/(3600*24*30));
+	duration %= (3600*24*30);
+	printf("  - %2ld days\n", duration/(3600*24));
+	duration %= (3600*24);
+	printf("  - %2ld hours\n", duration/(3600));
+	duration %= (3600);
+	printf("  - %2ld minutes\n", duration/(60));
+	duration %= (60);
+	printf("  - %2ld seconds\n", duration);
 
 	return 0;
 }
