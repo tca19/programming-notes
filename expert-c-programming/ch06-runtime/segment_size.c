6bb57ddb602d03f3d29b0b236c039de0b3639bef
tca19 <30314448+tca19@users.noreply.github.com>
2018-08-20 23:43:36 +0200

    solve programming challenge about segment sizes

---
+++ expert-c-programming/ch06-runtime/segment_size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*           Executable and Segment sizes
+ *
+ *  Basic Hello World!
+ *      - executable size : 16616 bytes
+ *      - segment sizes
+ *          - test : 1475
+ *          - data : 586
+ *          - bss  : 8
+ *          - dec  : 2067
+ *          - hex  : 813
+ */
+
+int main(void)
+{
+	printf("Hello World!\n");
+	return 0;
+}
