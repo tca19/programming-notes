b709c37d546daabb9e55974dbe44571bfb334681
tca19 <30314448+tca19@users.noreply.github.com>
2018-10-29 19:07:48 +0100

    add function to handle segfault from main

--- expert-c-programming/ch07-memory/seg_fault.c
+++ expert-c-programming/ch07-memory/seg_fault.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void catch_SEGFAULT(int sig)
+{
+	printf("ERROR: the code contains a SEGFAULT error. Exiting ...\n");
+	exit(1);
+}
+
 int main(void)
 {
+	signal(SIGSEGV, catch_SEGFAULT);
+
 	int *p = 0;
 	*p = 17;
 
