bd012ed8aa1a566cfd1c750e03084b97753f685a
tca19 <30314448+tca19@users.noreply.github.com>
2018-11-01 12:16:02 +0100

    solve exercise 6.06

---
+++ the-c-programming-language/ch06-structures/exercises/exercise6.06.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define HASHSIZE \
+	100
+
+struct nlist
+{
+	struct nlist *next;
+	char *name;
+	char *defn;
+};
+
+static struct nlist *hashtab[HASHSIZE];
+
+/* hash: produce hashtab index from string */
+unsigned int hash(char *s)
+{
+	unsigned int hashval;
+
+	for (hashval = 0; *s != '\0'; ++s)
+		hashval = *s + 31 * hashval;
+	return hashval % HASHSIZE;
+}
+
+/* strdup: duplicate string s */
+char *strdup(char *s)
+{
+	char *t;
+
+	t = malloc(strlen(s)+1);
+	if (t != NULL)
+		strcpy(t, s);
+	return t;
+}
+
+/* lookup: look for s in hashtab */
+struct nlist *lookup(char *s)
+{
+	struct nlist *p;
+
+	for (p = hashtab[hash(s)]; p != NULL; p = p->next)
+		if (strcmp(s, p->name) == 0)
+			return p;
+	return NULL;
+}
+
+/* install: add (name, defn) in hashtab */
+struct nlist *install(char *name, char *defn)
+{
+	struct nlist *p;
+	unsigned int hashval;
+
+	if ((p = lookup(name)) == NULL) /* not already here */
+	{
+		p = malloc(sizeof(*p));
+		hashval = hash(name);
+		p->next = hashtab[hashval];
+		p->name = strdup(name);
+		hashtab[hashval] = p;
+	}
+	else /* remove previous defn value for update */
+		free((void *) p->defn);
+
+	p->defn = strdup(defn);
+	return p;
+}
+
+/* undef; remove name if present in hashtab */
+void undef(char *name)
+{
+	struct nlist *p, *p_prev;
+	unsigned int hashval;
+
+	for (p = hashtab[hashval = hash(name)], p_prev = NULL;
+	     p != NULL;
+	     p_prev = p, p = p->next)
+		if (strcmp(name, p->name) == 0)
+		{
+			/* 2 cases : first elem of linked list , or not */
+			if (p_prev == NULL)
+				hashtab[hashval] = p->next;
+			else
+				p_prev->next = p->next;
+
+			free(p->name);
+			free(p->defn);
+			free(p);
+			break;
+		}
+}
+
+/* print_hashtab: print all (name, defn) stored in hashtab */
+void print_hashtab()
+{
+	struct nlist *p;
+	int i;
+
+	for (i = 0; i < HASHSIZE; ++i)
+		for (p = hashtab[i]; p != NULL; p = p->next)
+			printf("%s -> %s\n", p->name, p->defn);
+}
+
+#define BUFFERSIZE 100
+static char buffer[BUFFERSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+	return (bufp > 0) ? buffer[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFFERSIZE)
+		printf("error; ungetch() buffer is full.\n");
+	else
+		buffer[bufp++] = c;
+}
+
+/* getword: read a word from input to string word */
+int getword(char *word, int lim)
+{
+	int c;
+	char *w = word;
+
+	while (isspace(c = getch()));
+
+	if (c != EOF)
+		*w++ = c;
+	if (!isalpha(c) && c != '#' && c !=)
+	{
+		*w = '\0';
+		return c;
+	}
+
+	for (; --lim > 0; w++)
+	{
+		if (!isalnum(*w = getchar()))
+		{
+			ungetch(*w);
+			break;
+		}
+	}
+	*w = '\0';
+	return word[0];
+}
+
+int main()
+{
+	char word[100];
+	while (getword(word, 100) != EOF)
+		printf("%s\n", word);
+	return 0;
+}
